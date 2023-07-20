9648ddf73106d420d996d3d495c0926e89a44b8c
jennifer arevalo <arevalo5007@gmail.com>
2023-07-20 14:23:23 -0500

    add more questions

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const fs = require('fs');
 inquirer .prompt ()
 
 const questions = [
+ {
+    type:'input',
+    name:'Titel',
+    message:'What is your project name?'
+    
+    },
+    {
+        
+    type: 'checkbox',
+    message: 'What license would you like?',
+    name: 'license',
+    choices:['Artistic license 2.0', 'MIT','BSD 3-clause "New" or "Revised" license','Boost Software License 1.0'],
+
+    },
 {
     type: 'input',
     message: 'What is your Github User name?',
